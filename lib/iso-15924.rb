a8c2b7238da63f5c39a8670e26b182d8567ca397
webdev778 <webdev778@gmail.com>
2020-11-07 12:00:07 +0800

    Add basic methods and spec

--- lib/iso-15924.rb
+++ lib/iso-15924.rb
@@ -1,6 +1,29 @@
-require "iso/15924/version"
+require "iso-15924/version"
+require "yaml"
 
 module Iso15924
   class Error < StandardError; end
-    # Your code goes here...
+
+  ISO_15924 = lambda do
+    yaml_file = File.expand_path('iso-15924.yaml', File.dirname(__FILE__))
+    YAML.load(File.read(yaml_file)).tap do |h|
+      h.each do |k,v|
+        v.freeze
+      end
+    end
+  end.call.freeze
+
+  class << self
+    def codes
+      ISO_15924.keys
+    end
+
+    def valid?(code)
+      codes.include? code
+    end
+
+    def data
+      ISO_15924
+    end
+  end
 end
--- spec/iso-15924_spec.rb
+++ spec/iso-15924_spec.rb
@@ -3,7 +3,21 @@ RSpec.describe Iso15924 do
     expect(Iso15924::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has codes of ISO-15924" do
+    expect(Iso15924.codes.length).to be > 0
+  end
+
+  it "should be uniq" do
+    expect(Iso15924.codes.length).to be Iso15924.codes.uniq.length
+  end
+
+  it "should be immutable" do
+    expect { Iso15924.data["Adlm"] = {"custom": "data"} }.to raise_error(FrozenError)
+    expect { Iso15924.data["Adlm"]["numeric"] = 999 }.to raise_error(FrozenError)
+  end
+
+  it "validate code" do
+    expect(Iso15924.valid?("Adlm")).to be true
+    expect(Iso15924.valid?("XYZ")).to be false
   end
 end
